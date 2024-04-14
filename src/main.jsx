e7cb4743b9a1c1499d96522c5c6ef6c9404fcbe2
AAromero1 <aaromero1@uc.cl>
2024-04-14 11:06:26 -0400

    Arreglado lo que pide lint

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const Prueba = 123;
-const doble = 123;
-const triple = 123;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
