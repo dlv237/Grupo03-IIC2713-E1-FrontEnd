622c58ef7d302a872f0c5f4b3b35c93d7574b51c
dlv237 <dolobos@uc.cl>
2024-04-17 11:54:45 -0400

    Flights View

--- src/common/navbar.jsx
+++ src/common/navbar.jsx
@@ -3,19 +3,25 @@ import "./navbar.css";
 
 function Navbar() {
   return (
-      <ul className="navbar">
+    <ul className="navbar">
+      <li className="navbar-li">
+        <a href="/" className="navbar-a">
+          FlightsBooking
+        </a>
+      </li>
+      <div className="right-container">
         <li className="navbar-li">
-          <a href="/" className="navbar-a">FlightsBooking</a>
+          <a href="/about" className="navbar-a">
+            Iniciar Sesion
+          </a>
         </li>
-        <div className="right-container">
-            <li className="navbar-li">
-            <a href="/about" className="navbar-a">Iniciar Sesion</a>
-            </li>
-            <li className="navbar-li">
-            <a href="/contact" className="navbar-a">Registrarte</a>
-            </li>
-        </div>
-      </ul>
+        <li className="navbar-li">
+          <a href="/contact" className="navbar-a">
+            Registrarte
+          </a>
+        </li>
+      </div>
+    </ul>
   );
 }
 
--- src/flights/index.jsx
+++ src/flights/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import './index.css';
-import Button from '../common/Button.jsx';
+import "./index.css";
+import Button from "../common/Button.jsx";
 
 const Flights = () => {
   const [flights, setFlights] = useState([]);
-  const [arrivalFilter, setArrivalFilter] = useState('');
-  const [departureFilter, setDepartureFilter] = useState('');
+  const [arrivalFilter, setArrivalFilter] = useState("");
+  const [departureFilter, setDepartureFilter] = useState("");
   const [dateFilter, setDateFilter] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(25);
@@ -32,50 +32,95 @@ const Flights = () => {
   }, []);
 
   // Filtrar los vuelos
-  const filteredFlights = flights.filter(flight => {
-    const departureMatch = departureFilter ? flight.flights[0].departure_airport.id.includes(departureFilter) : true;
-    const arrivalMatch = arrivalFilter ? flight.flights[0].arrival_airport.id.includes(arrivalFilter) : true;
-    const dateMatch = dateFilter ? flight.flights[0].departure_airport.time.includes(dateFilter) : true;
+  const filteredFlights = flights.filter((flight) => {
+    const departureMatch = departureFilter
+      ? flight.flights[0].departure_airport.id.includes(departureFilter)
+      : true;
+    const arrivalMatch = arrivalFilter
+      ? flight.flights[0].arrival_airport.id.includes(arrivalFilter)
+      : true;
+    const dateMatch = dateFilter
+      ? flight.flights[0].departure_airport.time.includes(dateFilter)
+      : true;
 
     return departureMatch && arrivalMatch && dateMatch;
   });
 
   // Paginar los vuelos
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const selectedFlights = filteredFlights.slice(startIndex, startIndex + itemsPerPage);
+  const selectedFlights = filteredFlights.slice(
+    startIndex,
+    startIndex + itemsPerPage,
+  );
   const totalPages = Math.ceil(filteredFlights.length / itemsPerPage);
   return (
     <div className="container">
       <div className="filters">
-        <input type="text" value={departureFilter} onChange={e => setDepartureFilter(e.target.value)} placeholder="Departure" />
-        <input type="text" value={arrivalFilter} onChange={e => setArrivalFilter(e.target.value)} placeholder="Arrival" />
-        <input type="date" value={dateFilter} onChange={e => setDateFilter(e.target.value)} />
-        <input type="number" value={itemsPerPage} onChange={e => setItemsPerPage(Math.max(25, e.target.value))} min="25" placeholder="Items per page" />
-      </div>      
+        <input
+          type="text"
+          value={departureFilter}
+          onChange={(e) => setDepartureFilter(e.target.value)}
+          placeholder="Departure"
+        />
+        <input
+          type="text"
+          value={arrivalFilter}
+          onChange={(e) => setArrivalFilter(e.target.value)}
+          placeholder="Arrival"
+        />
+        <input
+          type="date"
+          value={dateFilter}
+          onChange={(e) => setDateFilter(e.target.value)}
+        />
+        <input
+          type="number"
+          value={itemsPerPage}
+          onChange={(e) => setItemsPerPage(Math.max(25, e.target.value))}
+          min="25"
+          placeholder="Items per page"
+        />
+      </div>
       {selectedFlights.map((flight, index) => {
-        const departureDate = new Date(flight.flights[0].departure_airport.time);
+        const departureDate = new Date(
+          flight.flights[0].departure_airport.time,
+        );
         const arrivalDate = new Date(flight.flights[0].arrival_airport.time);
-      
-        const departureTime = departureDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' });
-        let arrivalTime = arrivalDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' });
+
+        const departureTime = departureDate.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          timeZone: "UTC",
+        });
+        let arrivalTime = arrivalDate.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          timeZone: "UTC",
+        });
 
         const durationMs = arrivalDate - departureDate;
         const hours = Math.floor(durationMs / 3600000);
         const minutes = Math.round((durationMs % 3600000) / 60000);
         const duration = `${hours} horas ${minutes} minutos`;
-      
-        if (arrivalDate.setHours(0, 0, 0, 0) > departureDate.setHours(0, 0, 0, 0)) {
+
+        if (
+          arrivalDate.setHours(0, 0, 0, 0) > departureDate.setHours(0, 0, 0, 0)
+        ) {
           arrivalTime += " +1 día";
         }
         const totalPages = Math.ceil(filteredFlights.length / itemsPerPage);
         return (
-          <div key={index} className="flight-card"> 
+          <div key={index} className="flight-card">
             <div className="section-1">
               <h5>Duración estimada: {duration}</h5>
-              <h3>{flight.flights[0].departure_airport.id} ({departureTime})</h3>
+              <h3>
+                {flight.flights[0].departure_airport.id} ({departureTime})
+              </h3>
               <h4>{flight.flights[0].departure_airport.name}</h4>
               <div className="arrow">&darr;</div>
-              <h3>{flight.flights[0].arrival_airport.id} ({arrivalTime})</h3>
+              <h3>
+                {flight.flights[0].arrival_airport.id} ({arrivalTime})
+              </h3>
               <h4>{flight.flights[0].arrival_airport.name}</h4>
             </div>
             <div className="section-2">
@@ -83,27 +128,42 @@ const Flights = () => {
               <img src={flight.flights[0].airline_logo} alt="Airline logo" />
             </div>
             <div className="section-2">
-              <h4>Precio: {flight.price.toLocaleString('es-ES', { style: 'currency', currency: 'CLP' })} </h4>
-              <Button simple> Reservar  vuelo  </Button>
-              <br/>
+              <h4>
+                Precio:{" "}
+                {flight.price.toLocaleString("es-ES", {
+                  style: "currency",
+                  currency: "CLP",
+                })}{" "}
+              </h4>
+              <Button simple> Reservar vuelo </Button>
+              <br />
               <Button simple>Detalle del vuelo</Button>
             </div>
-            
           </div>
         );
       })}
       <div className="pagination">
-        <button onClick={() => setCurrentPage(oldPage => Math.max(oldPage - 1, 1))}>Anterior</button>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
+        <button
+          onClick={() => setCurrentPage((oldPage) => Math.max(oldPage - 1, 1))}
+        >
+          Anterior
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
           <button
             key={page}
             onClick={() => setCurrentPage(page)}
-            className={page === currentPage ? 'active' : ''}
+            className={page === currentPage ? "active" : ""}
           >
             {page}
           </button>
         ))}
-        <button onClick={() => setCurrentPage(oldPage => Math.min(oldPage + 1, totalPages))}>Siguiente</button>
+        <button
+          onClick={() =>
+            setCurrentPage((oldPage) => Math.min(oldPage + 1, totalPages))
+          }
+        >
+          Siguiente
+        </button>
       </div>
     </div>
   );
