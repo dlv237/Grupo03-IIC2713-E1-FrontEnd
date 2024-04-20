1f354202319dd31ab60f02bb7f0054ac948fa6d9
Fabian Mendoza <Femendoza@uc.cl>
2024-04-20 17:12:57 -0400

    Agregar rutas al router

--- src/common/router.jsx
+++ src/common/router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Landingpage from "../landingpage";
 import Login from "../userpages/login";
 import Register from "../userpages/register";
+import Flights from "../flights/index";
 
 const PageRoutes = () => {
   return (
@@ -9,6 +10,8 @@ const PageRoutes = () => {
       <Route path="/" element={<Landingpage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/flights" element={<Flights/>} />
+      <Route path="/flights/id" element={<Flights/>} />
     </Routes>
   );
 };
