8f2f1169dc11d48fbe369e4e78c5fab118ce334f
dlv237 <dolobos@uc.cl>
2024-04-24 22:24:07 -0400

    Auth0 Vistas Protegidas

--- src/flights/forbidden.jsx
+++ src/flights/forbidden.jsx
@@ -4,7 +4,7 @@ const Forbidden = () => {
     return (
         <div className='flight-container'>
             <div className='flight-card'>
-                <h1>Forbidden</h1>
+                <h1>Forbidden - </h1>
                 <h1>Access to this page is forbidden.</h1>
             </div>
         </div>
