cb3d0c4b425bb2921abb852048455cf7ee9e1df1
Fabian Mendoza <Femendoza@uc.cl>
2024-05-20 11:53:25 -0400

    Avance vista recomendaciones

--- src/flights/recomendations/recomendations.jsx
+++ src/flights/recomendations/recomendations.jsx
@@ -1,14 +1,26 @@
 import axios from "axios";
 import "./recomendations.css";
+import { useEffect, useState } from "react";
+import {useAuth0} from "@auth0/auth0-react";
 
 function Recomendation(){
 
-    <div>
-        <div className="left-recomendation-container">
+    const [recomendedFlights, setRecomendedFlights] = useState([]);
+    const { user } = useAuth0();
 
+    const GetRecomendedFlights = async () => {
+
+        const response = axios.get(`https://8ujhmk0td0.execute-api.us-east-2.amazonaws.com/Produccion3/flights/recomendations/${user.email}`);
+    }
+
+    <div className="recomendations-container">
+        <div className="left-recomendation-container">
+            <h1>Recomendaciones</h1>
+            <p>¿No sabes a donde ir? Aquí te dejamos algunas recomendaciones para que puedas elegir tu próximo destino.</p>
+            {/* lista de recomendaciones */}
         </div>
         <div className="right-recomendation-container">
-            
+            {/* tabla de informacion mas especifica del vuelo seleccionado */}
         </div>
     </div>
 
