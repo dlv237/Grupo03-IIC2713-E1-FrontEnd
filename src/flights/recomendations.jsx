48958441910019008ba44b7e2c51ab1152498db4
Fabian Mendoza <Femendoza@uc.cl>
2024-05-21 17:57:19 -0400

    Avance vista recomendaciones, aparecen detalles a la derecha

--- src/flights/recomendations.jsx
+++ src/flights/recomendations.jsx
@@ -7,18 +7,16 @@ import Button from "../common/button";
 
 function Recomendation() {
   const [recomendedFlights, setRecomendedFlights] = useState([]);
+  const [selectedFlight, setSelectedFlight] = useState(null);
   const { user } = useAuth0();
 
   useEffect(() => {
     const getRecomendedFlights = async () => {
       try {
         const response = await axios.get(
-          `https://8ujhmk0td0.execute-api.us-east-2.amazonaws.com/Produccion2/recommendation/${user.email}`,
+          `https://8ujhmk0td0.execute-api.us-east-2.amazonaws.com/Produccion2/recommendation/${user.email}`
         );
-        console.log("response", response);
         setRecomendedFlights(response.data.recommendations[0]);
-        console.log("Recomended Flights:", response.data.recommendations[0]);
-        console.log("first flight", response.data.recommendations[0][0]);
       } catch (error) {
         console.error("Error fetching user recommendations", error);
       }
@@ -29,6 +27,10 @@ function Recomendation() {
     }
   }, [user]);
 
+  const handleFlightClick = (flight) => {
+    setSelectedFlight(flight);
+  };
+
   return (
     <div className="recomendations-container">
       <div className="left-recomendation-container">
@@ -36,7 +38,7 @@ function Recomendation() {
         {recomendedFlights.length > 0 && (
           <div>
             <h2 className="recommendation-subtitle">Vuelo utilizado para la recomendación:</h2>
-            <div className="flight-cell">
+            <div className="flight-cell" onClick={() => handleFlightClick(recomendedFlights[0])}>
               <div>
                 <img className="recommendation-text" src={recomendedFlights[0].airlineLogo} alt="Airline Logo" />
                 <p className="recommendation-text">Airline: {recomendedFlights[0].flights[0].airline}</p>
@@ -56,11 +58,10 @@ function Recomendation() {
           <div>
             <h2 className="recommendation-subtitle">Vuelos recomendados:</h2>
             {recomendedFlights.slice(1).map((flight, index) => (
-              <div className="flight-cell" key={index}>
+              <div className="flight-cell" key={index} onClick={() => handleFlightClick(flight)}>
                 <div>
                   <img className="recommendation-text" src={flight.airlineLogo} alt="Airline Logo" />
                   <p className="recommendation-text">Airline: {flight.flights[0].airline}</p>
-                  <p className="recommendation-text">Airplane: {flight.flights[0].airplane}</p>
                   <p className="recommendation-text">Departure: {flight.flights[0].departure_airport.name}</p>
                   <p className="recommendation-text">Arrival: {flight.flights[0].arrival_airport.name}</p>
                   <p className="recommendation-text">Price: {flight.price} {flight.currency}</p>
@@ -75,7 +76,20 @@ function Recomendation() {
         )}
       </div>
       <div className="right-recomendation-container">
-        {/* detalles específicos del vuelo */}
+        {selectedFlight && (
+          <div className="flight-details">
+            <h2 className="recommendation-subtitle">Detalles del vuelo</h2>
+            <div className="flight-detail-cell">
+              <img className="recommendation-text" src={selectedFlight.airlineLogo} alt="Airline Logo" />
+              <p className="recommendation-text">Airline: {selectedFlight.flights[0].airline}</p>
+              <p className="recommendation-text">Departure: {selectedFlight.flights[0].departure_airport.name}</p>
+              <p className="recommendation-text">Arrival: {selectedFlight.flights[0].arrival_airport.name}</p>
+              <p className="recommendation-text">Price: {selectedFlight.price} {selectedFlight.currency}</p>
+              <p className="recommendation-text">Seats Available: {selectedFlight.seats_available}</p>
+              <p className="recommendation-text">Carbon Emission: {selectedFlight.carbonEmission.this_flight}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
