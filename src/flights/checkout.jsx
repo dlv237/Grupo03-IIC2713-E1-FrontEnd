987d80c0470b779748a75ead3fbd39f232e4a76f
Fabian Mendoza <Femendoza@uc.cl>
2024-05-26 02:12:36 -0400

    fixes boleta

--- src/flights/checkout.jsx
+++ src/flights/checkout.jsx
@@ -20,8 +20,8 @@ const Checkout = () => {
         const flightResponse = await axios.get(
           `https://8ujhmk0td0.execute-api.us-east-2.amazonaws.com/Produccion2/flights/${flightId}`
         );
-        setFlightDetails(flightResponse.data);
-        console.log('Flight details:', flightResponse.data);
+        setFlightDetails(flightResponse.data.flight);
+        console.log('Flight details:', flightResponse.data.flight);
       } catch (error) {
         console.error("Error fetching transaction:", error);
       }
