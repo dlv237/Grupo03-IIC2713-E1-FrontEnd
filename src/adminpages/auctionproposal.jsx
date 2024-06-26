f119bf72f06548f9d8e1fb3db424dd2bf13d0a3e
FabianMF1 <Femendoza@uc.cl>
2024-06-25 20:17:05 -0400

    feat: Add React import to adminauctions.jsx
    The code changes in adminauctions.jsx include adding the import statement for React, which was missing before. This import is necessary for using React hooks like useEffect and useState. This change ensures that the component functions correctly and avoids any potential errors related to missing imports.

--- src/adminpages/auctionproposal.jsx
+++ src/adminpages/auctionproposal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
-import Button from "../common/button.jsx";
+import Button from "../common/Button.jsx";
 
 const AuctionsWithProposals = () => {
     const [data, setData] = useState([]);
