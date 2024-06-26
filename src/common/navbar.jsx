b510123dacc6959bd94aedd7ee34fdd3579b2c65
AAromero1 <aaromero1@uc.cl>
2024-06-26 00:15:40 -0400

    SDAHJNK;FDHJNK;DSGBFA'

--- src/common/navbar.jsx
+++ src/common/navbar.jsx
@@ -6,7 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
   const { isAuthenticated, user } = useAuth0();
-  const isAdmin = user?.admin || false;
+  const isAdmin = user?.email === "admin@example.com";
 
   console.log("User:", user);
 
@@ -23,47 +23,51 @@ function Navbar() {
         </Link>
       </li>
       <div className="right-container">
-        <li className="navbar-li">
-          {isAuthenticated ? (
-            <>
-              <li className="navbar-li">
-                <Link to="/recomendations" className="navbar-a">
-                  ¿No sabes a donde ir?
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <Link to="/my_flights" className="navbar-a">
-                  Mis Vuelos
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <Link to="/admin/auctions" className="navbar-a">
-                  Ofertas de otros grupos
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <Link to="/admin/proposal" className="navbar-a">
-                  Propuestas
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <Link to="/admin-flights" className="navbar-a">
-                  Auctions
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <Link to="/settings" className="navbar-a">
-                  Configuraciones
-                </Link>
-              </li>
-              <li className="navbar-li">
-                <LogoutButton className="login-button"></LogoutButton>
-              </li>
-            </>
-          ) : (
-            <LoginButton className="navbar-a login-button"></LoginButton>
-          )}
-        </li>
+        {isAuthenticated ? (
+          <>
+            <li className="navbar-li">
+              <Link to="/recomendations" className="navbar-a">
+                ¿No sabes a donde ir?
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link to="/my_flights" className="navbar-a">
+                Mis Vuelos
+              </Link>
+            </li>
+            {isAdmin && (
+              <>
+                <li className="navbar-li">
+                  <Link to="/admin/auctions" className="navbar-a">
+                    Ofertas de otros grupos
+                  </Link>
+                </li>
+                <li className="navbar-li">
+                  <Link to="/admin/proposal" className="navbar-a">
+                    Propuestas
+                  </Link>
+                </li>
+                <li className="navbar-li">
+                  <Link to="/admin-flights" className="navbar-a">
+                    Auctions
+                  </Link>
+                </li>
+                <li className="navbar-li">
+                  <Link to="/settings" className="navbar-a">
+                    Configuraciones
+                  </Link>
+                </li>
+              </>
+            )}
+            <li className="navbar-li">
+              <LogoutButton className="login-button" />
+            </li>
+          </>
+        ) : (
+          <li className="navbar-li">
+            <LoginButton className="navbar-a login-button" />
+          </li>
+        )}
       </div>
     </ul>
   );
